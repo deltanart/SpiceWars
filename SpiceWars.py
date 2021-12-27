05f4f49c25afcb321b263f7884b57d6fe120287f
C. Neinert <54905826+CNeinert@users.noreply.github.com>
2021-12-27 10:29:57 +0100

    v0.1.1

--- SpiceWars.py
+++ SpiceWars.py
@@ -1,50 +1,67 @@
 import tkinter, random
 #------------------------------------
-#------- Beginnvariablen-------------
+#------- Basisvariablen-------------
 #------------------------------------
-Geld = 1000
-Laderaum = 10
-Gewuerze = ["Gewürze", "Ersatzteile", "Kyber-Kristalle", "Astromechs", "Raketenteile", "Treibstoff", "Sensorkomponenten", "Steuerungseinheiten","KI-Systeme","Werkzeuge","Durastahl","Energiedroiden","Rezeptoren","Nahrungsmittel"]
 
-aktKosten = [30, 60, 300 , 150 ,800,500,60,50,80,30,90,300,20,40]
-KostenMin = [25, 50 , 300 , 100, 600,400,40,50,70,10,70,250,13,20]
-KostenMax = [100, 180 , 1700 , 450, 2000,1000,100,90,122,200,120,600,70,250]
+Geld = 1000
+Laderaum = 100
+
+#Wahren
+Gewuerze = ["Gewürze", "Ersatzteile", "Kyber-Kristalle", "Astromechs", "Raketenteile", "Treibstoff",
+            "Sensorkomponenten", "Steuerungseinheiten","KI-Systeme","Werkzeuge","Durastahl",
+            "Energiedroiden","Rezeptoren","Nahrungsmittel"]
+aktKosten = [30,60,300,150,800,500,60,50,80,30,90,300,20,40]
+KostenMin = [25,50,300,100,600,400,40,50,70,10,70,250,13,20]
+KostenMax = [100,180,1700,450,2000,1000,100,90,122,200,120,600,70,250]
+
 EigeneLadung = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
 Ort = "Tatooine"
-HP = 1
+HP = 100
 MaxHP = 100
-SchiffeListe = ["A-Wing", "X-Wing", "BTL-A4 Y-Wing", "Slave I", "kleiner SternenjÃ¤ger", "AR-170", "P-38","T-2c Theta-Klasse Transporter", "Tantive IV", "Millenium Falke", "YT-1200", "TIE-X1", "T-4a Lambda-Klasse Transporter", "U-Wing", "SternenzerstÃ¶rer"]
+
+#Liste Schiffe 
+SchiffeListe = ["A-Wing", "X-Wing", "BTL-A4 Y-Wing", "Slave I", "kleiner SternenjÃ¤ger", "AR-170",
+                "P-38","T-2c Theta-Klasse Transporter", "Tantive IV", "Millenium Falke", "YT-1200",
+                "TIE-X1", "T-4a Lambda-Klasse Transporter", "U-Wing", "Sternenzerstörer"]
 LaderaumSchiffe =[100,120,150,300,150,180,200,500,1200,555,400,200,600,400,100000000]
 hpschiffe=[150,150,200,500,200,250,250,400,1000,1500,900,300,500,400,100000000]
 schiffpreis=[10000,20000,50000,100000,50000,60000,50000,110000,500000,200000,220000,30000,150000,100000,100000000]
-Info = ["Zurzeit keine Infos"]
+
+#Farbe
 Farbe = "navajowhite2"
-FarbePlanet = ["green2","navajowhite2","LightCyan2","Deepskyblue4","forestgreen","SpringGreen4","Slateblue4","orange","greenyellow","springgreen","deepskyblue","forestgreen","olivedrab","deepskyblue4","darkgoldenrod1","snow","linen","navajowhite2","chartreuse2","cadetblue4","gold","LightCyan2","gray60","gray32"]
+FarbePlanet = ["green2","navajowhite2","LightCyan2","Deepskyblue4","forestgreen","SpringGreen4",
+               "Slateblue4","orange","greenyellow","springgreen","deepskyblue","forestgreen",
+               "olivedrab","deepskyblue4","darkgoldenrod1","snow","linen","navajowhite2","chartreuse2",
+               "cadetblue4","gold","LightCyan2","gray60","gray32"]
+
 #-----------------------------------------------------------
 #------- Spielinhalte --- (Hintergrundfunktionen) ----------
 #-----------------------------------------------------------
-def LebenSchiff():
-  global HP
-  x = random.randint(1,100)
-  if Geld <=0 and EigeneLadung == [0,0,0,0,0,0,0,0,0,0,0,0,0,0]:
+
+def LebenSchiff():#neu
+  global HP, Geld, EigeneLadung
+  x = random.randint(1,3)
+  if Geld < 1 and EigeneLadung == [0,0,0,0,0,0,0,0,0,0,0,0,0,0]:  
     tkinter.messagebox.showerror("- G A M E  O V E R -","Sie sind pleite.\n\nDas Spiel ist vorbei...")
     NeuesSpiel()
   if HP > 0:
     HP = HP - 1
-  if x == 17 or x == 53:
-    HP = HP - random.randint(20,60)
+  if x == 1 or x == 2:
+    HP = HP - random.randint(HP//5,HP//2)
     tkinter.messagebox.showwarning("- SONNENSTURM -","Ihr Schiff hat Schaden genommen.\n")
-  if HP <= 0:
-    tkinter.messagebox.showerror("- G A M E  O V E R -","Sie wurden zerstört.\n\nDas Spiel ist vorbei...")
+  if HP < 1:
+    tkinter.messagebox.showerror("- G A M E  O V E R -","Sie wurden zerstÃ¶rt.\n\nDas Spiel ist vorbei...")
     NeuesSpiel()
 
   
-def Piraten():
+def Piraten():#neu
   global HP
+  global MaxHP
   global EigeneLadung
-  h = random.randint(1,40)
-  if h == 17:
-    HP = HP - random.randint(50,90)
+  h = random.randint(1,50)
+  if h == 25:
+    HP = HP - random.randint(30,90)
     tkinter.messagebox.showinfo("  - P I R A T E N ! -  ", "Du wurdest von Piraten überfallen und hast Schaden genommen.\n Alle deine Waren wurden gestohlen!")
     EigeneLadung = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   if HP <= 0:
@@ -52,23 +69,16 @@ def Piraten():
     NeuesSpiel()
 
 
-#def Zeit(): #Epochen oder ablaufende Zeit?
-  
-
 def DisplayAktualisieren():
-  global Gewuerze
-  global aktKosten
-  global EigeneLadung
-  global Geld
-  global Laderaum
-  global SchiffeListe, LaderaumSchiffe, hpschiffe, schiffpreis, Laderaum
-  global Info
-  global Farbe
+  global Gewuerze, aktKosten, EigeneLadung, Geld, Laderaum, SchiffeListe, LaderaumSchiffe, hpschiffe, schiffpreis, Laderaum, Info, Farbe, HP
+
+  #Farbwechsel
   Fenster.configure(bg=str(Farbe))
   LabelMenge.configure(bg=str(Farbe))
   LabelOrt.configure(bg=str(Farbe))
   LabelSchiffe.configure(bg=str(Farbe))
-  
+
+  #Listbox "Gewürze
   Liste.delete("0","end")
   Liste.insert("end", Gewuerze[0] + "  " + str(aktKosten[0]))
   Liste.insert("end", Gewuerze[1] + "  " + str(aktKosten[1]))
@@ -84,7 +94,8 @@ def DisplayAktualisieren():
   Liste.insert("end", Gewuerze[11] + "  " + str(aktKosten[11]))
   Liste.insert("end", Gewuerze[12] + "  " + str(aktKosten[12]))
   Liste.insert("end", Gewuerze[13] + "  " + str(aktKosten[13]))
-  
+
+  #Listbox Laderaum
   ListeLaderaum.delete("0","end")
   ListeLaderaum.insert("end", str(EigeneLadung[0]) + " Einheiten: " + str(Gewuerze[0])) 
   ListeLaderaum.insert("end", str(EigeneLadung[1]) + " Einheiten: " + str(Gewuerze[1]))
@@ -105,6 +116,7 @@ def DisplayAktualisieren():
   ListeLaderaum.insert("end", "Platz im Laderaum: " + str(Laderaum))
   ListeLaderaum.insert("end", "HP: " + str(HP))
 
+  #Listbox Schiffe
   ListeSchiffe.delete("0","end")
   ListeSchiffe.insert("end", str(SchiffeListe[0]) + " - " + "Laderaum: " + str(LaderaumSchiffe[0]) + " - " + " HP: " + str(hpschiffe[0]) + " - " + "Preis: " + str(schiffpreis[0]))
   ListeSchiffe.insert("end", str(SchiffeListe[1]) + " - " + "Laderaum: " + str(LaderaumSchiffe[1]) + " - " + " HP: " + str(hpschiffe[1]) + " - " + "Preis: " + str(schiffpreis[1]))
@@ -122,7 +134,7 @@ def DisplayAktualisieren():
   ListeSchiffe.insert("end", str(SchiffeListe[13]) + " - " + "Laderaum: " + str(LaderaumSchiffe[13]) + " - " + " HP: " + str(hpschiffe[13]) + " - " + "Preis: " + str(schiffpreis[13]))
   ListeSchiffe.insert("end", str(SchiffeListe[14]) + " - " + "Laderaum: " + str(LaderaumSchiffe[14]) + " - " + " HP: " + str(hpschiffe[14]) + " - " + "Preis: " + str(schiffpreis[14]))
 
-  
+  #Listbox Planeten
   ListeStaedte.delete("0","end")
   ListeStaedte.insert("end", "Alderaan")
   ListeStaedte.insert("end", "Tatooine")
@@ -149,6 +161,7 @@ def DisplayAktualisieren():
   ListeStaedte.insert("end", "Sullust")
   ListeStaedte.insert("end", "Todesstern")
 
+  #LabelOrt
   LabelOrt.config(text ="aktueller Ort: " + str(Ort))
   
 
@@ -156,17 +169,12 @@ def DisplayAktualisieren():
 #----------------------------------------------------------
 #----- Buttons --------------------------------------------
 #----------------------------------------------------------
+  
 def NeuesSpiel():
-  global aktKosten
-  global EigeneLadung
-  global Geld
-  global Laderaum
-  global Ort
-  global SchiffeListe, LaderaumSchiffe, hpschiffe, schiffpreis
-  global Info
-  global Farbe
-  global FarbePlanet
-  FarbePlanet = ["green2","navajowhite2","LightCyan2","Deepskyblue4","forestgreen","SpringGreen4","Slateblue4","orange","greenyellow","springgreen","deepskyblue","forestgreen","olivedrab","deepskyblue4","darkgoldenrod1","snow","linen","navajowhite2","chartreuse2","cadetblue4","gold","LightCyan2","gray60","gray32"]
+  global aktKosten, EigeneLadung, Geld, Laderaum, SchiffeListe, LaderaumSchiffe, hpschiffe, schiffpreis,Farbe, HP, Ort, FarbePlanet
+  FarbePlanet = ["green2","navajowhite2","LightCyan2","Deepskyblue4","forestgreen","SpringGreen4","Slateblue4","orange",
+                 "greenyellow","springgreen","deepskyblue","forestgreen","olivedrab","deepskyblue4","darkgoldenrod1",
+                 "snow","linen","navajowhite2","chartreuse2","cadetblue4","gold","LightCyan2","gray60","gray32"]
   aktKosten = [30, 60, 300 , 150 ,800,500,60,50,80,30,90,300,20,40]
   EigeneLadung = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   Geld = 1000
@@ -174,12 +182,13 @@ def NeuesSpiel():
   DisplayAktualisieren()
   Ort = "Tatooine"
   Farbe = "navajowhite2"
-  HP = 1
-  SchiffeListe = ["A-Wing", "X-Wing", "BTL-A4 Y-Wing", "Slave I", "kleiner SternenjÃ¤ger", "AR-170", "P-38","T-2c Theta-Klasse Transporter", "Tantive IV", "Millenium Falke", "YT-1200", "TIE-X1", "T-4a Lambda-Klasse Transporter", "U-Wing", "SternenzerstÃ¶rer"]
+  HP = 100
+  SchiffeListe = ["A-Wing", "X-Wing", "BTL-A4 Y-Wing", "Slave I", "kleiner SternenjÃ¤ger", "AR-170",
+                  "P-38","T-2c Theta-Klasse Transporter", "Tantive IV", "Millenium Falke", "YT-1200",
+                  "TIE-X1", "T-4a Lambda-Klasse Transporter", "U-Wing", "SternenzerstÃ¶rer"]
   LaderaumSchiffe =[100,120,150,300,150,180,200,500,1200,555,400,200,600,400,100000000]
   schiffpreis=[10000,20000,50000,100000,50000,60000,50000,110000,500000,200000,220000,30000,150000,100000,100000000]
   hpschiffe=[150,150,200,500,200,250,250,400,1000,1500,900,300,500,400,100000000]
-  Info = ["Zurzeit keine Infos"]
   DisplayAktualisieren()
 
 def kaufen():
@@ -194,7 +203,6 @@ def kaufen():
       Geld = Geld - int(aktKosten[Nummer])*Anzahl
       EigeneLadung[Nummer] = EigeneLadung[Nummer]+Anzahl
   DisplayAktualisieren()
-
 
 def verkaufen():
   global Geld
@@ -220,24 +228,19 @@ def Weitersegeln():
   global KostenMax
   Nummer = int(ListeStaedte.curselection()[0])
   Stadt = ListeStaedte.get(Nummer)
-  Farbe = FarbePlanet[Nummer]
-  Ort = str(Stadt)
+  Farbe = FarbePlanet[Nummer]#neu
+  Ort = str(Stadt)#neu
   tkinter.messagebox.showinfo("- R E I S E I N F O -","Ihre Reise geht nach " + Stadt )
   for i in range (14):
     aktKosten[i] =  random.randint(KostenMin[i],KostenMax[i])
-  Piraten()
-  LebenSchiff()
+  Piraten()#neu
+  LebenSchiff()#neu
   DisplayAktualisieren()
 
-def Schiffe():
-  global Geld
-  global schiffpreis
-  global LaderaumSchiffe
-  global EigeneLadung
-  global ListeSchiffe
-  global Laderaum
-  global MaxHP
-  global HP
+def Schiffe():#neu
+  global Geld, MaxHP, HP, Laderaum
+  global schiffpreis, LaderaumSchiffe, EigeneLadung, ListeSchiffe
+
   Nummer = int(ListeSchiffe.curselection()[0])
   if (Geld >= schiffpreis[Nummer]):
       Laderaum = int(LaderaumSchiffe[Nummer])
@@ -247,7 +250,7 @@ def Schiffe():
       EigeneLadung = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   DisplayAktualisieren()
 
-def Reparatur():
+def Reparatur():#neu
   global HP
   global MaxHP
   global Geld
@@ -255,8 +258,10 @@ def Reparatur():
     HP = MaxHP
     Geld = Geld - 50
   DisplayAktualisieren()
-#------------------ GUI -------------------------------------------------------------------------------------
+  
+#--------------------- GUI ----------------------------------------------------------------------------------
 #----------------- Hauptfenster -----------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------
 
   
 Fenster = tkinter.Tk()
@@ -318,8 +323,8 @@ ButtonVerkaufen.grid(padx=5, pady = 5, row =3, column = 2, columnspan=2)
 ButtonBewegen = tkinter.Button(Fenster,fg="cyan",bg="gray17", text = ' Sprung in den Hyperraum ', command = Weitersegeln)
 ButtonBewegen.grid(row=4, column=2, padx=5, pady=25, columnspan = 2)
 
-ButtonSchiffe = tkinter.Button(Fenster,fg="snow",bg="gray17", text = ' Schiff kaufen ', command = Schiffe)
-ButtonSchiffe.grid(row=5, column=2, padx=5, pady=25, columnspan = 2)
+ButtonSchiffeKaufen = tkinter.Button(Fenster,fg="snow",bg="gray17", text = ' Schiff kaufen ', command = Schiffe)
+ButtonSchiffeKaufen.grid(row=5, column=2, padx=5, pady=25, columnspan = 2)
 
 ButtonNeustart = tkinter.Button(Fenster,fg="snow",bg="gray17" ,text = ' neues Spiel ', command = NeuesSpiel)
 ButtonNeustart.grid(row=0, column=1, padx=5, pady=10)
